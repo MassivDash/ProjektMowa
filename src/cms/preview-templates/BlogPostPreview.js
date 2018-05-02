9f326046e1a75eae2aa155c8713b87ddb694c2ee
MassivDash <lukasz.celitan@gmail.com>
2018-05-02 12:57:22 +0200

    added the data needed for cms preview

--- src/cms/preview-templates/BlogPostPreview.js
+++ src/cms/preview-templates/BlogPostPreview.js
@@ -20,12 +20,64 @@ const BlogPostPreview = ({ entry, widgetFor }) => (
         title: entry.getIn(['data', 'title']),
         thumbnail: entry.getIn(['data', 'thumbnail' ]),
         date: entry.getIn(['data', 'date']),
-
-
-      }
-      
+      }  
 
       }}
+
+    OtherPosts={{
+      edges: [
+        {node: {
+          children: [],
+          excerpt: "Wiecej tekstu ............",
+          fields: {
+            slug: "/blog/2018-04-29-kolejne-niesmowity-wpis/"
+          }, 
+          frontmatter: {
+            title: "Kolejne niesamowity wpis",
+            thumbnail: "/img/aron-visuals-54965-unsplash.jpg",
+            description: "i kolejny niesamowity fajny krotki opis",
+            templateKey: "blog-post",
+            date: "Kwiecień 29, 2018"
+          }
+        } 
+        
+        }, 
+        {node: {
+          children: [],
+          excerpt: "Warto Zaczac od naglowka (przycisk H1) potem mamy h2 na mniejsze tytuly sekcji **Zgrubienie tekstu ** Kursywa tekst normalny  cytat: o ktorym jeszcze nie slyszalas  lista dssd fsd numerowana lista  jeden dwa aby dodac zdjecie wcisnij plus > image (warto wpisywac alt tekst) tagi, odzielamy przecinkiem np:  logopedia, radio, zycie",
+          fields: {
+            slug: "/blog/2018-04-29-swietny-pomysł-na-wpis/"
+          }, 
+          frontmatter: {
+            title: "Swietny pomysł na wpis",
+            thumbnail: "/img/marcus-castro-309376-unsplash.jpg",
+            description: "A to jest swietny ktortki opis ktory pojawia się na takze na glownej stronie (slider)",
+            templateKey: "blog-post",
+            date: "Kwiecień 29, 2018"
+          }
+        } 
+        
+        },
+        {node: {
+          children: [],
+          excerpt: "Warto Zaczac od naglowka (przycisk H1) potem mamy h2 na mniejsze tytuly sekcji **Zgrubienie tekstu ** Kursywa tekst normalny  cytat: o ktorym jeszcze nie slyszalas  lista dssd fsd numerowana lista  jeden dwa aby dodac zdjecie wcisnij plus > image (warto wpisywac alt tekst) tagi, odzielamy przecinkiem np:  logopedia, radio, zycie",
+          fields: {
+            slug: "/blog/2018-04-29-logopedia-w-radiu/"
+          }, 
+          frontmatter: {
+            title: "Logopedia",
+            thumbnail: "/img/bogomil-mihaylov-519207-unsplash.jpg",
+            description: "A to jest swietny ktortki opis ktory pojawia się na takze na glownej stronie (slider)",
+            templateKey: "blog-post",
+            date: "Kwiecień 29, 2018"
+          }
+        } 
+        
+        }
+
+
+      ]
+    }}
   />
 )
 
