302eb3bde7f5e2cc78033dae64123231e7ffd0ad
MassivDash <lukasz.celitan@gmail.com>
2018-05-06 23:27:52 +0200

    small changes to cms preview

--- src/templates/cmsblog-post.js
+++ src/templates/cmsblog-post.js
@@ -117,7 +117,7 @@ BlogPost.propTypes = {
 export default BlogPost
 
 export const pageQuery = graphql`
-  query BlogPostByID($id: String!) {
+  query CMSBlogPostByID($id: String!) {
     markdownRemark(id: { eq: $id }) {
       id
       html
