48904a6bfecca660af2ae886216e289c481539db
MassivDash <lukasz.celitan@gmail.com>
2018-05-07 01:03:55 +0200

    cms fighting

--- src/cms/preview-templates/CMSBlogPostPreview.js
+++ src/cms/preview-templates/CMSBlogPostPreview.js
@@ -4,30 +4,17 @@ import { CMSBlogPostTemplate } from '../../templates/cmsblog-post'
 
 
 const CMSBlogPostPreview = ({ entry, widgetFor }) => (
-
-
   
  <CMSBlogPostTemplate
     
     title={entry.getIn(['data', 'title'])}
     content={widgetFor('body')}
     thumbnail={entry.getIn(['data', 'thumbnail' ])}
-    postPath={entry.getIn(['data', 'slug'])}
     slug={entry.getIn(['data', 'slug'])}
     tags={entry.getIn(['data', 'tags'])}
     description={entry.getIn(['data', 'description'])}
-    helmet={entry.getIn(['data', 'title'])}
     date={entry.getIn(['data', 'date'])}
-    postNode={{
-      frontmatter: {
-        description: entry.getIn(['data', 'description']),
-        tags: entry.getIn(['data', 'tags']),
-        title: entry.getIn(['data', 'title']),
-        thumbnail: entry.getIn(['data', 'thumbnail' ])  
-      }
-      
-
-      }}
+   
   />
 )
 
